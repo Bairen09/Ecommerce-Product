bad5c36b6281820b64ae1369bab7562474cfabf5
bairan <byerain009@gmail.com>
2025-10-21 18:40:33 +0530

    Made Product controller,service and repo layer we can now get all products from the database and also get a particular product from a particular category by their categoryID.

---
+++ src/main/java/com/ecom/productcatalog/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.ecom.productcatalog.controller;
+
+import com.ecom.productcatalog.model.Product;
+import com.ecom.productcatalog.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProduct(){
+        return productService.getAllProduct();
+    }
+    @GetMapping("/category/{categoryId}")
+    public List<Product>getProductByCategory(@PathVariable Long categoryId){
+        return productService.getProductByCategory(categoryId);
+    }
+}
